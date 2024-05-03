1a4f0440e7cdf56c888e911eb3d2f9b2dc33c81c
Flurysz <ilya.shastitko@gmail.com>
2024-05-04 00:17:09 +0200

    Added class Student and options to add student and write out students

--- StudentyZnamky/Program.cs
+++ StudentyZnamky/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 namespace StudentyZnamky
 {
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int StudentID { get; set; }
+        public Student(string name, string lastname, int studentID)
+        {
+            Name = name;
+            Lastname = lastname;
+            StudentID = studentID;
+        }
+
+        /*public static void WriteLine(Student student)
+        {
+            Console.WriteLine($"{student.StudentID} {student.Name} {student.Lastname}");
+        }*/
+
+        public static Student AddStudent()
+        {
+            Console.Write("Zadejte jmeno: ");
+            string name = Console.ReadLine();
+            Console.Write("Zadejte prijmeni: ");
+            string lastname = Console.ReadLine();
+            Console.Write("Zadejte ID studenta: ");
+            int studentID = int.Parse(Console.ReadLine());
+
+            return new Student(name, lastname, studentID);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> studentList = new List<Student>();
+
+            char answer;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Pridat studenta [p]");
+                Console.WriteLine("Vypsat studenty [v]");
+                Console.WriteLine("To quit [q]");
+                answer = char.ToLower(Console.ReadKey().KeyChar);
+
+                switch (answer)
+                {
+                    case 'p':
+                        Console.Clear();
+                        studentList.Add(Student.AddStudent());
+                        break;
+                    case 'v':
+                        Console.Clear();
+                        foreach (Student student in studentList)
+                            Console.WriteLine($"{student.StudentID} {student.Name} {student.Lastname}");
+                        Console.ReadKey();
+                        break;
+                    case 'q':
+                        break;
+                    default:
+                        break;
+                }
+                if (answer == 'q')
+                    break;
+            }
         }
     }
 }
